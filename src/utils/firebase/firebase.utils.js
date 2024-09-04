e9a6571d9b97715bf2990a8ee3ddca6800cb3c22
SarEnderTarkanson <atarkan@hotmail.com>
2024-09-04 13:06:04 +0200

    corrected a bug in createUserWithEmailAndPassword

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -60,5 +60,5 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
 
-  createUserWithEmailAndPassword(auth, email, password);
+  return await createUserWithEmailAndPassword(auth, email, password);
 };
