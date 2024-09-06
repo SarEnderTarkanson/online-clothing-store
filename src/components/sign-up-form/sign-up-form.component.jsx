6af43b5e97cb304e715af702ce6ef7ce6ddfe5f5
SarEnderTarkanson <atarkan@hotmail.com>
2024-09-06 12:31:59 +0200

    added the context API to children components

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Button from "../button/button.component";
 import FormInput from "../form-input/form-input.component";
-
+import { UserContext } from "../../contexts/user.context";
 
 import {
   createAuthUserWithEmailAndPassword,
@@ -21,6 +21,8 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
+  const { setCurrentUser } = useContext(UserContext);
+
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
@@ -37,6 +39,9 @@ const SignUpForm = () => {
         email,
         password
       );
+
+      setCurrentUser(user)
+
       await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
     } catch (error) {
