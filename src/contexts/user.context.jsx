5a18a20414f8f22e041107ae0c95dd2c385aaf6a
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-12-25 04:49:34 +0100

    converted the cart context's logic into using reducers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useReducer } from 'react';
+import { createAction } from '../utils/reducer/reducer.utils.js';
 
 import {
   onAuthStateChangedListener,
@@ -36,7 +37,7 @@ export const UserProvider = ({ children }) => {
   const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
   const { currentUser } = state;
   const setCurrentUser = (user) => {
-    dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+    dispatch(createAction( USER_ACTION_TYPES.SET_CURRENT_USER, user))
   }
   const value = { currentUser, setCurrentUser };
 
